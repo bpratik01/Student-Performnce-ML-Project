fb655d23c3c5ed0aaf03ac99821592fabb692b34
Pratik <pratikrbokade05@gmailcom>
2024-07-25 13:51:45 +0530

    made required changes and practiced logging. The logger is working fine now and logs are being generated

--- src/logger.py
+++ src/logger.py
@@ -8,14 +8,13 @@ LOG_PATH.mkdir(exist_ok=True, parents=True)
 
 LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
 
-
 LOG_FILE_PATH = LOG_PATH / LOG_FILE
 
 logging.basicConfig(
-  filename=LOG_FILE_PATH,
-  format= '%(asctime)s:%(%(name)s:%(levelname)s:%(message)s:)',
-  level=logging.INFO
+    filename=LOG_FILE_PATH,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
 )
 
 if __name__ == '__main__':
-  logging.info('Logging has started')
+    logging.info('Logging has started')
