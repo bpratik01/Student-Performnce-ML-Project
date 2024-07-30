f0d63a8b6ebecb9794a2c634335d46d334ac8077
Pratik <pratikrbokade05@gmailcom>
2024-07-30 14:40:45 +0530

    Performed EDA and begin with the model building notebook

--- src/exception.py
+++ src/exception.py
@@ -4,7 +4,7 @@ def error_message_detail(error, error_message):
   _,_, exc_tb = sys.exc_info() #this give 3 placeholders first 2 are not that useful, 3rd is the traceback object  that helps us to get the line number of the error
   
   file_name = exc_tb.tb_frame.f_code.co_filename
-  error_message = f'Error Occured in file: {file_name} at line number: {exc_tb.tb_lineno} with error message: {error_message}'
+  error_message = f'Error Occured in this file: {file_name} at line number: {exc_tb.tb_lineno} with error message: {error_message}'
   
   return error_message
 
@@ -18,3 +18,4 @@ class CustomException(Exception):
     
   def __str__(self):
     return self.error_message
+  
